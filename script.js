33b4c1490121316d2bc911a4d418cf5b0cc90626
Jeremy Manalo <jrmanalo91@yahoo.com>
2017-05-18 01:18:10 -0500

    part 3 intro to js

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ for (let i = 1; i <= 20; i++)
     console.log(value);
 }
 
+//Step 3.
+//Write just the countBs function (and the code needed to test it)
+//described on page 56 ("Bean Counting") in your text.
+
+function countBs(string)
+{
+    var count = 0;
+    for(i = 0; i < string.length; i++)
+    {
+        if (string.charAt(i) === "B")
+        {
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+console.log("Counting all uppercase Bs");
+	countBs("ball is life");
+	countBs("I like Big Butts and I cannot lie");
+	countBs("BumbleBees Bite Brown Boogers");
